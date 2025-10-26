5d2e3f8825a8a980c0fc784ea127948b04d596e9
karblum27 <kbluma@INF.UG.EDU.PL>
2025-10-26 10:32:40 +0100

    Program kalkulator

---
+++ kalkulator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+int a=10;
+int b=5;
+int c;
+int d;
+int e;
+int f;
+
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+
+printf("Dodawanie a i b: %d\n", c);
+printf("Odejmowanie a i b: %d\n", d);
+printf("Mnożenie a i b: %d\n", e);
+printf("Dzielenie a i b: %d\n", f);
+
+  return 0;
+}
