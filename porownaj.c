dd809ce9ca677a38be75cdbc2374287d4c46f767
karblum27 <kbluma@INF.UG.EDU.PL>
2025-10-26 11:38:16 +0100

    Program porownujacy liczby

---
+++ porownaj.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+int a=10;
+int b=20;
+if (a > b)
+	printf("Pierwsza liczba (%d) jest większa od drugiej liczby (%d).\n", a, b);
+    } else if (b > a) {
+        printf("Druga liczba (%d) jest większa od pierwszej liczby (%d).\n", b, a);
+    } else {
+        printf("Obie liczby (%d i %d) są równe.\n", a, b);
+
+
+  return 0;
+}
